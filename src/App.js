4b7eea2e7cb99e08c0f0708d0216ac53939ceae5
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2023-01-17 22:15:23 +0100

    fade in, footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import ScrollToTop from "react-scroll-to-top";
 import LandingPage from "./Pages/LandingPage"
 import "./App.css";
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 function App() {
   return (
     <div className="App">
--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,133 +1,193 @@
 import React from "react";
 import HeadFoot from "../component/HeadFoot";
+import SlickSlider from "../component/slickSlider/SlickSlider";
 import { Button } from "@mui/material";
-
-
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 export default function LandingPage() {
   return (
     <HeadFoot>
-        <div className="carousel_container">
-          <div
-            id="carouselExampleIndicators"
-            className="carousel slide"
-            data-ride="carousel"
-          >
-            <ol className="carousel-indicators">
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="0"
-                className="active indicator"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="1"
-                className="indicator"
-              ></li>
-              <li
-                data-target="#carouselExampleIndicators"
-                data-slide-to="2"
-                className="indicator"
-              ></li>
-            </ol>
-            <div className="carousel-inner">
-              <div class="carousel-item active">
-                <div className="carousel-content">
-                  <div className="details">
-                  <Button variant="contained" className="hero-btn light-btn">SUPER SALE</Button>
+      <div className="carousel_container">
+        <div
+          id="carouselExampleIndicators"
+          className="carousel slide"
+          data-ride="carousel"
+        >
+          <ol className="carousel-indicators">
+            <li
+              data-target="#carouselExampleIndicators"
+              data-slide-to="0"
+              className="active indicator"
+            ></li>
+            <li
+              data-target="#carouselExampleIndicators"
+              data-slide-to="1"
+              className="indicator"
+            ></li>
+            <li
+              data-target="#carouselExampleIndicators"
+              data-slide-to="2"
+              className="indicator"
+            ></li>
+          </ol>
+          <div className="carousel-inner">
+            <div class="carousel-item active">
+              <div className="carousel-content">
+                <div className="details">
+                  <Button variant="contained" className="hero-btn light-btn"           data-aos="slide-down"
+          data-aos-offset="10"
+          data-aos-delay="30"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          
+          data-aos-once="false">
+                    SUPER SALE
+                  </Button>
 
-                    <h1 className="title-hero">
-                      Best Online Medical Shopping Now
-                    </h1>
-                    <p>
-                      Nulla vitae elit libero, a pharetra augue mollis interdum.
-                    </p>
-                    <Button variant="contained" className="hero-btn main-btn">
-                      Read more
-                    </Button>
-                  </div>
-                  <div className="image">
-                    <img
-                      className="d-block w-100 carousel-image"
-                      src=""
-                      alt="First slide"
-                    />
-                  </div>
+                  <h1 className="title-hero" 
+                  data-aos="zoom-in"
+                  data-aos-offset="10"
+                  data-aos-delay="30"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-once="false">
+                    Best Online Medical Shopping Now
+                  </h1>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                  <Button variant="contained" className="hero-btn main-btn" slide-up
+                                   data-aos="slide-up"
+                                   data-aos-offset="10"
+                                   data-aos-delay="30"
+                                   data-aos-duration="1000"
+                                   data-aos-easing="ease-in-out"
+                                   data-aos-once="false"
+                  >
+                    Read more
+                  </Button>
                 </div>
-              </div>
-              <div class="carousel-item">
-                <div className="carousel-content">
-                  <div className="details">
-                  <Button variant="contained" className="hero-btn light-btn">SUPER SALE</Button>
-                    <h1 className="title-hero">
-                    Best Online Medical Shopping Now                    </h1>
-                    <p>
-                      Nulla vitae elit libero, a pharetra augue mollis interdum.
-                    </p>
-                    <Button variant="contained" className="hero-btn main-btn">
-                      Read more
-                    </Button>
-                  </div>
-                  <div className="image">
-                    <img
-                      className="d-block w-100 carousel-image"
-                      src=""
-                      alt="First slide"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <div className="carousel-content">
-                  <div className="details">
-                    <Button variant="contained" className="hero-btn light-btn">SUPER SALE</Button>
-                    <h1 className="title-hero">
-                    Best Online Medical Shopping Now                    </h1>
-                    <p>
-                      Nulla vitae elit libero, a pharetra augue mollis interdum.
-                    </p>
-                    <Button variant="contained" className="hero-btn main-btn">
-                      Read more
-                    </Button>
-                  </div>
-                  <div className="image">
-                    <img
-                      className="d-block w-100 carousel-image"
-                      src=""
-                      alt="First slide"
-                    />
-                  </div>
+                <div className="image">
+                  <img
+                    className="d-block w-100 carousel-image"
+                    src=""
+                    alt="First slide"
+                  />
                 </div>
               </div>
             </div>
-            <a
-              className="carousel-control-prev"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a
-              class="carousel-control-next"
-              href="#carouselExampleIndicators"
-              role="button"
-              data-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="sr-only">Next</span>
-            </a>
+            <div class="carousel-item">
+              <div className="carousel-content">
+                <div className="details">
+                  <Button variant="contained" className="hero-btn light-btn">
+                    SUPER SALE
+                  </Button>
+
+                  <h1 className="title-hero">
+                    Best Online Medical Shopping Now
+                  </h1>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                  <Button variant="contained" className="hero-btn main-btn">
+                    Read more
+                  </Button>
+                </div>
+                <div className="image">
+                  <img
+                    className="d-block w-100 carousel-image"
+                    src=""
+                    alt="First slide"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div className="carousel-content">
+                <div className="details">
+                  <Button variant="contained" className="hero-btn light-btn">
+                    SUPER SALE
+                  </Button>
+
+                  <h1 className="title-hero">
+                    Best Online Medical Shopping Now
+                  </h1>
+                  <p>
+                    Nulla vitae elit libero, a pharetra augue mollis interdum.
+                  </p>
+                  <Button variant="contained" className="hero-btn main-btn">
+                    Read more
+                  </Button>
+                </div>
+                <div className="image">
+                  <img
+                    className="d-block w-100 carousel-image"
+                    src=""
+                    alt="First slide"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="prev"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-slide="next"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
+      </div>
 
       <main>
-        <section className="section-a">
+        <section className="section-a"
+                          data-aos="slide-up"
+                          data-aos-offset="10"
+                          data-aos-delay="30"
+                          data-aos-duration="1000"
+                          data-aos-easing="ease-in-out"
+                          data-aos-once="false"
+        >
+          <div className="left">
+            <div
+              className="section-description"
+            ></div>
+            <div className="img">
+              <img src="" alt="product" />
+            </div>
+          </div>
+          <div className="right">
+            <h5>Free Shipping</h5>
+            <h3>30% OFF</h3>
+            <p>
+              Your team is great. They are truly outstanding and understand the
+              equipment 100%. It's worth doing business with you.
+            </p>
+          </div>
+        </section>
+        <section
+          className="section-b"
+          data-aos="fade-up"
+          data-aos-offset="10"
+          data-aos-delay="10"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-once="false"
+        >
           <div className="left">
             <div className="section-description"></div>
             <div className="img">
@@ -137,14 +197,17 @@ export default function LandingPage() {
           <div className="right">
             <h5>Free Shipping</h5>
             <h3>30% OFF</h3>
-<p>
-
-Your team is great. They are truly outstanding and understand the equipment 100%. It's worth doing business with you.</p>
+            <p>
+              Your team is great. They are truly outstanding and understand the
+              equipment 100%. It's worth doing business with you.
+            </p>
           </div>
         </section>
-        <section className="section-b">
-          <div></div>
-          <div></div>
+
+        <section className="featured-products">
+          <h5>Feature</h5>
+          <h3>Featured Products</h3>
+          {/* <SlickSlider/> */}
         </section>
       </main>
     </HeadFoot>
