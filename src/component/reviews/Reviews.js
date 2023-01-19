2b51053e7bed393bf9c91e7bec397c145bc5e0cc
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2023-01-19 18:19:41 +0100

    style

--- src/component/reviews/Reviews.js
+++ src/component/reviews/Reviews.js
@@ -10,7 +10,9 @@ export default function Reviews() {
       <hr/>
 
       <div className="reviewer">
-        <div className="reviewer-img"></div>
+        <div className="reviewer-img">
+          <img src="/images/4-2.png" alt="user" />
+        </div>
         <div className="reveiwer-info">
           <h4 className="name">Dr. Gbenga Showunmi</h4>
           <p className="job">Web Developer</p>
